93f6c0c935600945f41859179e848493737a7560
Michael Chang <qzhang@groupon.com>
2019-11-25 13:48:22 -0600

    Split authenticate function

--- pkg/oidc/oidc.go
+++ pkg/oidc/oidc.go
@@ -28,6 +28,13 @@ type Authenticator struct {
 
 	payload string
 	done    chan error
+
+	tokenVerifier            *oidc.IDTokenVerifier
+	state                    string
+	oauthConfig              oauth2.Config
+	codeChallengeParam       oauth2.AuthCodeOption
+	codeChallengeMethodParam oauth2.AuthCodeOption
+	codeVerifierParam        oauth2.AuthCodeOption
 }
 
 // Payload holds tokens after authentication
@@ -46,6 +53,57 @@ func (a *Authenticator) failed(w http.ResponseWriter, err error) {
 	a.done <- err
 }
 
+func (a *Authenticator) callbackHandler(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Query().Get("state") != a.state {
+		a.failed(w, errors.New("state did not match"))
+		return
+	}
+
+	oauth2Token, err := a.oauthConfig.Exchange(r.Context(), r.URL.Query().Get("code"), a.codeVerifierParam)
+	if err != nil {
+		a.failed(w, err)
+		return
+	}
+	rawIDToken, ok := oauth2Token.Extra("id_token").(string)
+	if !ok {
+		a.failed(w, errors.New("No id_token field in oauth2 token"))
+		return
+	}
+	_, err = a.tokenVerifier.Verify(r.Context(), rawIDToken)
+	if err != nil {
+		a.failed(w, err)
+		return
+	}
+
+	payload := Payload{
+		IDToken:     rawIDToken,
+		AccessToken: oauth2Token.AccessToken,
+	}
+	payloadString, err := json.Marshal(payload)
+	if err != nil {
+		a.failed(w, err)
+	}
+	a.payload = string(payloadString)
+	a.succeeded(w)
+	return
+}
+
+func (a *Authenticator) generateState(len int) string {
+	randomBytes := make([]byte, len)
+	rand.Read(randomBytes)
+	return base64.RawURLEncoding.EncodeToString(randomBytes)[:len]
+}
+
+func (a *Authenticator) generateCodeVerifier(len int) (string, string) {
+	randomBytes := make([]byte, len)
+	rand.Read(randomBytes)
+	codeVerifier := base64.RawURLEncoding.EncodeToString(randomBytes)[:len]
+	h := sha256.New()
+	h.Write([]byte(codeVerifier))
+	codeChallenge := base64.RawURLEncoding.EncodeToString(h.Sum(nil))
+	return codeVerifier, codeChallenge
+}
+
 // Authenticate with IDP
 func (a *Authenticator) Authenticate(ctx context.Context) (payload string, err error) {
 	a.done = make(chan error)
@@ -57,69 +115,28 @@ func (a *Authenticator) Authenticate(ctx context.Context) (payload string, err e
 	oidcConfig := &oidc.Config{
 		ClientID: a.ClientID,
 	}
-	verifier := provider.Verifier(oidcConfig)
-	config := oauth2.Config{
+
+	a.tokenVerifier = provider.Verifier(oidcConfig)
+	a.oauthConfig = oauth2.Config{
 		ClientID:    a.ClientID,
 		Endpoint:    provider.Endpoint(),
 		RedirectURL: a.RedirectURL,
 		Scopes:      []string{oidc.ScopeOpenID, "profile"},
 	}
 
-	// Generate random state
-	randomBytes := make([]byte, 64)
-	rand.Read(randomBytes)
-	state := base64.RawURLEncoding.EncodeToString(randomBytes)[:64]
+	a.state = a.generateState(64)
+	codeVerifier, codeChallenge := a.generateCodeVerifier(128)
 
-	// Generate PKCE verifier and challenge
-	randomBytes = make([]byte, 128)
-	rand.Read(randomBytes)
-	codeVerifier := base64.RawURLEncoding.EncodeToString(randomBytes)[:128]
-	h := sha256.New()
-	h.Write([]byte(codeVerifier))
-	codeChallenge := base64.RawURLEncoding.EncodeToString(h.Sum(nil))
-	codeChallengeParam := oauth2.SetAuthURLParam("code_challenge", codeChallenge)
-	codeChallengeMethodParam := oauth2.SetAuthURLParam("code_challenge_method", "S256")
-	codeVerifierParam := oauth2.SetAuthURLParam("code_verifier", codeVerifier)
+	a.codeChallengeParam = oauth2.SetAuthURLParam("code_challenge", codeChallenge)
+	a.codeChallengeMethodParam = oauth2.SetAuthURLParam("code_challenge_method", "S256")
+	a.codeVerifierParam = oauth2.SetAuthURLParam("code_verifier", codeVerifier)
 
 	u, err := url.Parse(a.RedirectURL)
 	if err != nil {
 		return
 	}
 
-	http.HandleFunc(u.Path, func(w http.ResponseWriter, r *http.Request) {
-		if r.URL.Query().Get("state") != state {
-			a.failed(w, errors.New("state did not match"))
-			return
-		}
-
-		oauth2Token, err := config.Exchange(ctx, r.URL.Query().Get("code"), codeVerifierParam)
-		if err != nil {
-			a.failed(w, err)
-			return
-		}
-		rawIDToken, ok := oauth2Token.Extra("id_token").(string)
-		if !ok {
-			a.failed(w, errors.New("No id_token field in oauth2 token"))
-			return
-		}
-		_, err = verifier.Verify(ctx, rawIDToken)
-		if err != nil {
-			a.failed(w, err)
-			return
-		}
-
-		payload := Payload{
-			IDToken:     rawIDToken,
-			AccessToken: oauth2Token.AccessToken,
-		}
-		payloadString, err := json.Marshal(payload)
-		if err != nil {
-			a.failed(w, err)
-		}
-		a.payload = string(payloadString)
-		a.succeeded(w)
-		return
-	})
+	http.HandleFunc(u.Path, a.callbackHandler)
 
 	listener, err := net.Listen("tcp", a.ListenAddress)
 	if err != nil {
@@ -131,7 +148,7 @@ func (a *Authenticator) Authenticate(ctx context.Context) (payload string, err e
 	}()
 
 	go func() {
-		if err := browser.OpenURL(config.AuthCodeURL(state, codeChallengeParam, codeChallengeMethodParam)); err != nil {
+		if err := browser.OpenURL(a.oauthConfig.AuthCodeURL(a.state, a.codeChallengeParam, a.codeChallengeMethodParam)); err != nil {
 			a.done <- err
 		}
 	}()
