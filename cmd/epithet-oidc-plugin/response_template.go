ccbca3318eaef8fbbc0a60e0db88019231f8876d
Michael Chang <qzhang@groupon.com>
2020-03-16 17:08:09 -0500

    Add bootstrap css

--- cmd/epithet-oidc-plugin/response_template.go
+++ cmd/epithet-oidc-plugin/response_template.go
@@ -9,9 +9,10 @@ const responseTpl = `
 <html>
 	<head>
 		<meta charset="UTF-8">
+		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
 		<title>{{ $title }}</title>
 	</head>
-	<body>
+	<body class="container">
 		<h1>{{ $title }}</h1>
 	{{ if .Error }}
 		<h3>Try again after correcting the error below</h3>
